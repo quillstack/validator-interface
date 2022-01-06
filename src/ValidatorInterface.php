490de9c18c320ff51d7027a2909a765113c31114
Radek Ziemniewicz <radek@quillstack.org>
2022-01-06 21:15:26 +0000

    Removed .scrutinizer.yml

--- src/ValidatorInterface.php
+++ src/ValidatorInterface.php
@@ -12,8 +12,6 @@ interface ValidatorInterface
     /**
      * Validate method only validates. It doesn't set anything, you can implement your own setters, or pass your
      * parameters via construct. It should return true/bool value, or throw an exception in case of failure.
-     *
-     * @return bool
      */
     public function validate(): bool;
 }
